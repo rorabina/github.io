cf90c3862bd663d2c8860bc80ed3df54c241d8aa
rjrabina <54608576+rjrabina@users.noreply.github.com>
2026-09-12 09:09:34 +0000

    auto: update PWA cache name and asset precache list [skip ci]

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Keep your dynamic build hash here!
-const CACHE_NAME = 'ror-pwa-110a5441edba1e49e0acc3d07731721d391c065e';
+const CACHE_NAME = 'ror-pwa-667ba76997cf1bc9c80cef808402a91566797e70';
 
 // Comprehensive list of all site pages and core assets to precache
 const PRECACHE_ASSETS = [
